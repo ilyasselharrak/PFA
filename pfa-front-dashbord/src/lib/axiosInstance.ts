3c7081129d3bc8f75d0d009915a5e75adbe3ef15
ILYASSEHARRAK <eilyass913@gmail.com>
2026-06-19 19:05:00 +0100

    feat: Update axios instance base URL to use HTTPS

--- pfa-front-dashbord/src/lib/axiosInstance.ts
+++ pfa-front-dashbord/src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosError } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: "http://pfa-production-7b1b.up.railway.app",
+  baseURL: "https://pfa-production-7b1b.up.railway.app",
   headers: { "Content-Type": "application/json" },
 });
 
@@ -56,7 +56,7 @@ api.interceptors.response.use(
 
       try {
         const response = await axios.post<{ accessToken: string; refreshToken: string }>(
-          "http://pfa-production-7b1b.up.railway.app/auth/refresh-token",
+          "https://pfa-production-7b1b.up.railway.app/auth/refresh-token",
           { refreshToken }
         );
 
